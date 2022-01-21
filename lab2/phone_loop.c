ab05e9aba5cf86018403c88c9e15d13e492b46e1
Chong-Cheng Wang <ccjay.wang@mail.utoronto.ca>
2022-01-21 17:08:31 -0500

    new method of finding EOF.

--- lab2/phone_loop.c
+++ lab2/phone_loop.c
@@ -4,34 +4,27 @@ int main(int argc, char **argv) {
 
     char phone[11];
     scanf("%s", phone);
-
-
-    /*
-        I found the feof function on a stackoverflow question that I since been unable to find, however the question was related
-        to how to tell when standard input closes.
-        Here's a link on what feof does though; in essense, it checks if a specific datastream is currently at the the end-of-file, 
-        and returns 1 or 0.
-
-        stdin is standard input stream.
-
-        https://www.tutorialspoint.com/c_standard_library/c_function_feof.htm
-    */
-    while(!feof(stdin)){
+    int running = 1;
+    while(running){
         int op;
         int succ = scanf("%d", &op);
 
-        if (succ != 0){
-            if (op == -1){
-            printf("%s\n", phone);
-            }
-            else if (op >= 0  && op <= 9){
-                printf("%c\n", phone[op]);
-            }
-            else{
-                printf("ERROR\n");
-            }
+        if (succ == EOF){
+            running = 0;
         }
         else{
+            if (succ != 0){
+                if (op == -1){
+                printf("%s\n", phone);
+                }
+                else if (op >= 0  && op <= 9){
+                    printf("%c\n", phone[op]);
+                }
+                else{
+                    printf("ERROR\n");
+                }
+            }
+            else{
             /* found on StackOverflow: https://stackoverflow.com/questions/11805414/why-does-scanf-not-wait-for-user-input-after-it-fails-one-time
         
             A behaviour with scanf seems to be that if you give it an invalid input (ie, scanf looking for int and you give it a string), it will "clog up" the input
@@ -41,7 +34,10 @@ int main(int argc, char **argv) {
             */
             char junk[256];
             scanf("%s", junk);
+            printf("ERROR\n");
+            }
         }
     }
+
     return 0;
 }

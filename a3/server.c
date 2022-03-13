f121e335d3c8bd1c949122dca3272e3230610401
wangc470 <markus@markus.com>
2022-03-12 19:49:16 -0500

    Update files

---
+++ a3/server.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <getopt.h>
+
+// The maximum length of an HTTP message line
+#define MAX_LINE 256
+// The maximum length of an HTTP response message
+#define MAX_LENGTH 16*1024
+// The size of a chunk of HTTP response to read from the pipe
+#define CHUNK_SIZE 1024
+
+
+void printError(char *);
+void printServerError(char *str);
+void printResponse(char *str);
+
+int debug = 0;
+
+
+int main(int argc, char **argv) {
+    char msg[MAX_LENGTH];
+    int result;
+
+    FILE *fp = stdin; // default is to read from stdin
+
+    // Parse command line options.
+    int opt;
+    while((opt = getopt(argc, argv, "v")) != -1) {
+        switch(opt) {
+            case 'v':
+                debug = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-v] [filename]\n", argv[0]);
+                exit(1);
+        }
+    }
+    if(optind < argc) {
+        if((fp = fopen(argv[optind], "r")) == NULL) {
+            perror("fopen");
+            exit(1);
+        }
+    }
+
+
+    // TODO Complete program
+
+    if(fp != stdin) {
+        if(fclose(fp) == EOF) {
+            perror("fclose");
+            exit(1);
+        }
+    }
+}
+
+
+/* Print an http error page  
+ * Arguments:
+ *    - str is the path to the resource. It does not include the question mark
+ * or the query string.
+ */
+void printError(char *str) {
+    printf("HTTP/1.1 404 Not Found\r\n\r\n");
+
+    printf("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n");
+    printf("<html><head>\n");
+    printf("<title>404 Not Found</title>\n");
+    printf("</head><body>\n");
+    printf("<h1>Not Found</h1>\n");
+    printf("The requested resource %s was not found on this server.\n", str);
+    printf("<hr>\n</body></html>\n");
+}
+
+
+/* Prints an HTTP 500 error page 
+ */
+void printServerError() {
+    printf("HTTP/1.1 500 Internal Server Error\r\n\r\n");
+
+    printf("<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n");
+    printf("<html><head>\n");
+    printf("<title>500 Internal Server Error</title>\n");
+    printf("</head><body>\n");
+    printf("<h1>Internal Server Error</h1>\n");
+    printf("The server encountered an internal error or\n");
+    printf("misconfiguration and was unable to complete your request.<p>\n");
+    printf("</body></html>\n");
+}
+
+
+/* Prints a successful response message
+ * Arguments:
+ *    - str is the output of the CGI program
+ */
+void printResponse(char *str) {
+    printf("HTTP/1.1 200 OK\r\n\r\n");
+    printf("%s", str);
+}

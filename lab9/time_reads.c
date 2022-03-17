19be03932b41fb99e4171ce91594d80622162000
Chong-Cheng Wang <ccjay.wang@mail.utoronto.ca>
2022-03-16 21:14:20 -0400

    Lab9

--- lab9/time_reads.c
+++ lab9/time_reads.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <time.h>
 
 /* Message to print in the signal handling function. */
 #define MESSAGE "%ld reads were done in %ld seconds.\n"
@@ -16,6 +17,12 @@
 long num_reads, seconds;
 
 
+void handler(int code){
+    // printf("caught SIGPROF, %d\n", code);
+    printf(MESSAGE, num_reads, seconds);
+    exit(0);
+}
+
 /* The first command-line argument is the number of seconds to set a timer to run.
  * The second argument is the name of a binary file containing 100 ints.
  * Assume both of these arguments are correct.
@@ -24,7 +31,7 @@ long num_reads, seconds;
 int main(int argc, char **argv) {
     if (argc != 3) {
         fprintf(stderr, "Usage: time_reads s filename\n");
-        exit(1);
+        exit(0);
     }
     seconds = strtol(argv[1], NULL, 10);
 
@@ -34,14 +41,53 @@ int main(int argc, char **argv) {
       exit(1);
     }
 
+    struct sigaction newact;
+    newact.sa_handler = handler;
+    newact.sa_flags = 0;
+    sigemptyset(&newact.sa_mask);
+    sigaction(SIGPROF, &newact, NULL);
+
     /* In an infinite loop, read an int from a random location in the file,
      * and print it to stderr.
      */
+
+    //seeding rng
+    unsigned int seed = clock();
+    srandom(seed);
+
+    int fd = fileno(fp);
+
+    int maxSize = lseek(fd, 0, SEEK_END);
+    int readData;
+    int readLoc;
+
+    num_reads = 0;
+
+    //setting timer
+    struct timeval timeValInt;
+    timeValInt.tv_usec = 0;
+    timeValInt.tv_sec = 0;
+
+    struct timeval timeValDur;
+    timeValDur.tv_sec = seconds;
+    timeValDur.tv_usec = 0;
+
+    struct itimerval timerStruct;
+    timerStruct.it_interval = timeValInt;
+    timerStruct.it_value = timeValDur;
+
+    int res = setitimer(ITIMER_PROF, &timerStruct, NULL);
+
+    if (res == -1){
+        perror("setitimer");
+        exit(1);
+    }
+
     for (;;) {
-
-
-
-
+        readLoc = random() % maxSize;
+        lseek(fd, readLoc, SEEK_SET);
+        read(fd, &readData, sizeof(int));
+        num_reads++;
     }
     return 1; // something is wrong if we ever get here!
 }
--- lab9/write_test_file.c
+++ lab9/write_test_file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* Write 100 integers (in binary) to a file with the name given by the command-line
  * argument.  You can write them from 0 to 99 or use random to write random numbers.
@@ -8,22 +9,28 @@
  */
 
 int main(int argc, char **argv) {
+    
+    unsigned int seed = clock();
+    srandom(seed);
+
     if (argc != 2) {
         fprintf(stderr, "Usage: write_test_file filename\n");
         exit(1);
     }
 
     FILE *fp;
-    if ((fp = fopen(argv[1], "w")) == NULL) {
+    if ((fp = fopen(argv[1], "w")) == NULL) { //if we're doing this in binary, shouldn't we be using 'wb'?
         perror("fopen");
         exit(1);
     }
 
     // TODO: complete this program according its description above.
-
-
-
-
+    
+    for (int i = 0; i < 100; i++)
+    {
+        int randInt = random() % 100;
+        write(fileno(fp), &randInt, sizeof(i));
+    }
 
     fclose(fp);
     return 0;
